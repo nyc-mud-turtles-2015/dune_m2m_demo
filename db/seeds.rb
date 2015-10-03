b4381cd20d3b7d2f3c64c98100e3c3c679adcb53
Steven G. Harms <steven@devbootcamp.com>
2015-10-03 18:27:09 -0400

    Add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+AWESOME_BOOKS = ["Dune", "Dune Messiah", "Children of Dune", "God Emperor of Dune"]
+COOL_CHARACTERS = [
+  "Paul Muad'Dib Atriedes",
+  "Princess Irulan",
+  "Chani",
+  "Saint Alia of the Knife Atriedes",
+  "Lady Jessica",
+  "Feyd-Rautha Harkonnen",
+  "Baron Vladimir Harkonnen",
+]
+
+CHILDREN_OF_DUNE = ["Leto II Atriedes", "Ghanima Atriedes" ]
+
+AWESOME_BOOKS.each do |title|
+  Book.create!(title: title, author: "Frank Herbert")
+end
+
+COOL_CHARACTERS.each do |name|
+  Character.create!(name: name)
+end
+
+dune = Book.first
+dune.characters << Character.all
+
+CHILDREN_OF_DUNE.each do |name|
+  Character.create!(name: name)
+end
+
+dm = Book.find_by(title: "Children of Dune")
+dm.characters << Character.where(name: CHILDREN_OF_DUNE)
