98537ee527fd01de06a3ef8aa996db8275965fdf
Steven G. Harms <steven@devbootcamp.com>
2015-10-04 06:59:51 -0400

    Arc.of: custom finder based to title, book name strings

--- app/models/appearance.rb
+++ app/models/appearance.rb
@@ -2,4 +2,8 @@ class Appearance < ActiveRecord::Base
   belongs_to :book
   belongs_to :character
   has_one :arc
+
+  def self.of(char_name = nil, args = {})
+    find_by(book: Book.find_by(title: args[:in]), character: Character.find_by(name: char_name))
+  end
 end
