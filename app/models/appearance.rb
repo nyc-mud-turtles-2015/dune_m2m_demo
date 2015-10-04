fa19a74855054fab4cc95f28667b8f9589bb624c
Steven G. Harms <steven@devbootcamp.com>
2015-10-04 06:14:31 -0400

    Add Appearance join model for has_many :through M2M

---
+++ app/models/appearance.rb
@@ -0,0 +1,4 @@
+class Appearance < ActiveRecord::Base
+  belongs_to :book
+  belongs_to :character
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151003221829) do
+ActiveRecord::Schema.define(version: 20151004101408) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "appearances", force: :cascade do |t|
+    t.integer  "book_id"
+    t.integer  "character_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  add_index "appearances", ["book_id"], name: "index_appearances_on_book_id", using: :btree
+  add_index "appearances", ["character_id"], name: "index_appearances_on_character_id", using: :btree
 
   create_table "books", force: :cascade do |t|
     t.string   "title",      null: false
@@ -23,8 +33,18 @@ ActiveRecord::Schema.define(version: 20151003221829) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "books_characters", id: false, force: :cascade do |t|
+    t.integer "book_id"
+    t.integer "character_id"
+  end
+
+  add_index "books_characters", ["book_id"], name: "index_books_characters_on_book_id", using: :btree
+  add_index "books_characters", ["character_id"], name: "index_books_characters_on_character_id", using: :btree
+
   create_table "characters", force: :cascade do |t|
     t.string "name", null: false
   end
 
+  add_foreign_key "appearances", "books"
+  add_foreign_key "appearances", "characters"
 end
